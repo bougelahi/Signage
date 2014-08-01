9ad22e88b8842e837a2a2344a1827bfe923d959d
bougelahi <bougelahi@gmail.com>
2014-08-01 12:21:31 -0400

    Bug Fixes
    Added some fixes, ready to test on the pi.

--- Signage.java
+++ Signage.java
@@ -1,7 +1,6 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -15,12 +14,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.concurrent.ExecutionException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -29,107 +26,117 @@ import javax.swing.Timer;
 
 
 public class Signage {
-	
-		String schedule = "";
-		BufferedImage[] imgs;
-		String[] imageNames;
-		
-		boolean doneDownloading = false;
-		SlideShowFrame frame;
-		int currentImage = 0;
-		BufferedImage image;
-		static final int IMG_WIDTH = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
-		static final int IMG_HEIGHT = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+
+	String schedule = "";
+	BufferedImage[] imgs;
+	String[] imageNames;
+
+	boolean doneDownloading = false;
+	SlideShowFrame frame;
+	int currentImage = 0;
+	BufferedImage image;
+	static final int IMG_WIDTH = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+	static final int IMG_HEIGHT = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 	public Signage() {
 		ActionListener imageUpdater = new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				imageUpdate();
 			}
-		};
+		};//end imageUpdater
 		Timer timer = new Timer(600000,imageUpdater);
 		timer.setRepeats(true);
 		timer.start();
 		imageUpdate();
 		frame = new SlideShowFrame();
-		
+
 	}
-	
-	
+
+
 	public void imageUpdate() {
 		try {
 			System.out.println("Download Schedule!");
 			downloadSchedule();
-			
-	
-		}
+
+
+		}//end try
 		catch(Exception e) {
 			e.printStackTrace();
-		}
+		}//end catch
 		try {
+			
 			System.out.println("Download Images!");
 			downloadImages();
-		}
+		}//end try
 		catch(Exception e) {
-			
-		}
-		
-	}
-	
+
+		}//end catch
+
+	}//end imageUpdate
+
 	BufferedImage resizeImage(BufferedImage originalImage, int type) {
 		BufferedImage resizedImage = new BufferedImage(IMG_WIDTH,IMG_HEIGHT,type);
 		Graphics2D g = resizedImage.createGraphics();
 		g.drawImage(originalImage,0,0,IMG_WIDTH,IMG_HEIGHT,null);
-		
+
 		return resizedImage;
-	}
-	
-	
+	}//end resizeImage
+
+
 	public void downloadSchedule() {
-		String url = "http://fsa-web02.food.uga.edu/signage/fetch_images.php";
+		
 		ArrayList<String> imageNameList = new ArrayList<String>();
 		try {
-			HttpURLConnection con = (HttpURLConnection)(new URL(url).openConnection());
+			//URL url = new URL("http://fsa-web02.food.uga.edu/signage/fetch_images.php");
+			URL url = new URL("http://128.192.109.15/signage/fetch_images.php");
+			HttpURLConnection con = (HttpURLConnection)(url.openConnection());
 			int responseCode = con.getResponseCode();
 			System.out.println("Sending 'GET' request to URL : " + url);
 			System.out.println("Response Code : " + responseCode);
-			
-			
+
+
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
-			
+
 			while((inputLine = in.readLine()) != null) {
-			//	System.out.println(inputLine);
+				//System.out.println(inputLine);
 				response.append(inputLine+"\r\n");
 				String[] arr = inputLine.split(",");
+				if(arr.length > 1 && arr[0].length() > 1) {
+				//System.out.println(arr[0].substring(1) + " added to imageNames");
 				imageNameList.add(arr[0].substring(1));
-				
-			}
-			
+				}
+
+			}//end while
+
 			in.close();
 			System.out.println(response.toString());
 			schedule = response.toString();
 			BufferedWriter writer = null;
 			writer = new BufferedWriter( new FileWriter("schedule.txt"));
-		    writer.write(response.toString());
-		    
-		    if ( writer != null)
-		        writer.close( );
-			imageNames = new String[imageNameList.size()];
-		    imageNameList.toArray(imageNames);
-		    
-		   
-		  
-			
-		} catch (MalformedURLException e) {
+			writer.write(response.toString());
+
+			if ( writer != null) {
+				writer.close( );
+			}//end if
+		imageNames = new String[imageNameList.size()];
+			imageNameList.toArray(imageNames);
+
+
+
+
+		}//end try 
+		catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
+		}//end MalformedURLException catch
+		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	}
-	
+		}//end IOExcception catch
+		
+	}//end downloadSchedule
+
 	public void downloadImages() {
 		doneDownloading = false;
 		SwingWorker worker = new SwingWorker<BufferedImage[], Void>() {
@@ -140,184 +147,120 @@ public class Signage {
 				for(int i = 0; i < imageNames.length; i++) {
 					try {
 						System.out.println("Writing image: " + imageNames[i].substring(imageNames[i].lastIndexOf("/")+1));
-						URL url = new URL("http://fsa-web02.food.uga.edu/signage" +imageNames[i]);
+						//URL url = new URL("http://fsa-web02.food.uga.edu/signage" +imageNames[i]);
+						URL url = new URL("http://128.192.109.15/signage" +imageNames[i]);
 						BufferedImage image = ImageIO.read(url);
 						type = image.getType() == 0? BufferedImage.TYPE_INT_ARGB : image.getType();
 						temp[i] = resizeImage(image,type); //TODO: Resize
 						if(imageNames[i].lastIndexOf("/") != -1) {
 							ImageIO.write(temp[i], "jpg", new File(imageNames[i].substring(imageNames[i].lastIndexOf("/")+1))); 
-						}
-						
-						
-					}
+						}//end if
+
+
+					}//end try
 					catch(Exception e) {
-						//e.printStackTrace();
-					}
-				}
-				
-				
+						e.printStackTrace();
+					}//end catch
+				}//end for
+
+
 				return temp;
-			}
-			
+			}//end doInBackground
+
 			@Override
 			public void done() {
-				
-					System.out.println("Writing done");
+
+				System.out.println("Writing done");
 				//	imgs = get();
-					doneDownloading = true;
-					imgs=getCurrentContent();
-					
-				
-			
-			}
-			
-		};
+				doneDownloading = true;
+				imgs=getCurrentContent();
+
+
+
+			}//end done
+
+		};//end worker
 		worker.execute();
-	}
-	
+	}//end downloadImages
+
 	public static void main(String[] args) {
 		Signage f = new Signage();
 		f.downloadSchedule();
-	/*
-		String input = "./uploads/Mkt_dining_on_MP.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/MKT_coupon_program.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/Mkt_gift_o_grams.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/MKT_catering-2.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/MKT_growingly_green.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/MKT_Georgios.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/scott_hall_promo.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/Marketing_RateSheet.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/dawgpause_tate.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/campus_eateries_list.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/Mkt_retail_enews.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/Checkindealsblue.jpg,12,0,30,0,0,1,1,1,1,1,0,\n"
-				+ "./uploads/MKT_2.jpg,12,0,30,0,0,1,1,1,1,1,0,";
-		
-		String[] temp = input.split("\n");
-		String[][] rows = new String[20][20];
-		for(int i = 0; i < temp.length; i++) {
-			attrs = temp[i].split(",");
-			
-			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-			Calendar calendar = Calendar.getInstance();
-			int day = calendar.get(Calendar.DAY_OF_WEEK); 
-			
 
-				int startHour = Integer.parseInt(attrs[1]);
-				if(startHour == 12) {
-					startHour =0;
-				}
-				int startMinute = Integer.parseInt(attrs[2]);
-				int endHour = Integer.parseInt(attrs[3]);
-				int endMinute = Integer.parseInt(attrs[4]);
-				
-				
-				int currentDay = Integer.parseInt(attrs[3+day]);
-				String currentTime = sdf.format(new Date());
-				int currentHour = Integer.parseInt(currentTime.substring(0, currentTime.indexOf(":")));
-				int currentMinute = Integer.parseInt(currentTime.substring(currentTime.indexOf(":") + 1));
-				
-				// (currentHour > startHour AND currentHour < endHour) OR
-				// (currentHour = startHour AND currentMinute >= startMinute) OR
-				// (currentHour = startHour AND currentHour = endHour AND currentMinute >= startMinute AND currentMinute <= endMinute)
-		
-			if(currentDay == 1 && (currentHour > startHour && currentHour < endHour) || (currentHour == startHour && currentHour == endHour && currentMinute >= startMinute && currentMinute <= endMinute)) {
-					
-				System.out.println(i+ " SlideShow is playing!");
-			}
-			else {
-				System.out.println("Current Day= " + currentDay);
-				System.out.println("Current time= " + currentHour+":"+currentMinute);
-				System.out.println("Start time= " + startHour+":"+startMinute);
-				System.out.println("End time= " + endHour+":"+endMinute);
-			}
-				
-		
-		
-		
-		}
-		
-		*/	
-		
-		
-		
-		
-	}
-	
+	}//end main
+
 	public BufferedImage[] getCurrentContent() {
 		ArrayList<BufferedImage> imageList = new ArrayList<BufferedImage>();
+		ArrayList<String> names = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("schedule.txt"));
 			String line;
 			String[] attrs;
 			int i = 0;
-		
-			while ((line = br.readLine()) != null) {
-			//	System.out.println(line);
-				attrs = line.split(",");
-				
 
+			while ((line = br.readLine()) != null) {
+				//	System.out.println(line);
+				attrs = line.split(",");
+
+				if(attrs.length >= 5) {
 				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 				Calendar calendar = Calendar.getInstance();
 				int day = calendar.get(Calendar.DAY_OF_WEEK); 
-				System.out.println("Current Day: " + day);
+			//	System.out.println("Current Day: " + day);
 
-					int startHour = Integer.parseInt(attrs[1]);
-				
-					int startMinute = Integer.parseInt(attrs[2]);
-					int endHour = Integer.parseInt(attrs[3]);
-					int endMinute = Integer.parseInt(attrs[4]);
-					
-					
-					int currentDay = Integer.parseInt(attrs[4+day]);
-					String currentTime = sdf.format(new Date());
-					int currentHour = Integer.parseInt(currentTime.substring(0, currentTime.indexOf(":")));
-					//currentHour = 9;
-					int currentMinute = Integer.parseInt(currentTime.substring(currentTime.indexOf(":") + 1));
-					
-					// (currentHour > startHour AND currentHour < endHour) OR
-					// (currentHour = startHour AND currentMinute >= startMinute) OR
-					// (currentHour = startHour AND currentHour = endHour AND currentMinute >= startMinute AND currentMinute <= endMinute)
-			
-				if(currentDay == 1 && (currentHour > startHour && currentHour < endHour) || (currentHour == startHour && currentHour == endHour && currentMinute >= startMinute && currentMinute <= endMinute)) {
-						
-					System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " SlideShow is playing!");
+				int startHour = Integer.parseInt(attrs[1]);
+
+				int startMinute = Integer.parseInt(attrs[2]);
+				int endHour = Integer.parseInt(attrs[3]);
+				int endMinute = Integer.parseInt(attrs[4]);
+
+
+				int currentDay = Integer.parseInt(attrs[4+day]);
+				String currentTime = sdf.format(new Date());
+				int currentHour = Integer.parseInt(currentTime.substring(0, currentTime.indexOf(":")));
+				int currentMinute = Integer.parseInt(currentTime.substring(currentTime.indexOf(":") + 1));
+
+				// (currentHour > startHour AND currentHour < endHour) OR
+				// (currentHour = startHour AND currentMinute >= startMinute) OR
+				// (currentHour = startHour AND currentHour = endHour AND currentMinute >= startMinute AND currentMinute <= endMinute)
+				//System.out.println("Current Time: " + currentHour +":"+currentMinute);
+				if(currentDay == 1 && (currentHour > startHour && currentHour < endHour) || (currentHour == startHour && currentMinute >= startMinute) || (currentHour == startHour && currentHour == endHour && currentMinute >= startMinute && currentMinute <= endMinute)) {
+
+					System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " IS playing!");
 					if(attrs[0].lastIndexOf("/") != -1)
-					imageList.add(ImageIO.read(new File(attrs[0].substring(attrs[0].lastIndexOf("/")+1))));
+					//	System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1));
+						imageList.add(ImageIO.read(new File(attrs[0].substring(attrs[0].lastIndexOf("/")+1))));
+					names.add(attrs[0].substring(attrs[0].lastIndexOf("/")+1));
 					
 				}
+
+				}//end if
 				else {
-				
-				  System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " is not playing!");
-					/*
-				  System.out.println("Current Day= " + currentDay);
-					System.out.println("Current time= " + currentHour+":"+currentMinute);
-					System.out.println("Start time= " + startHour+":"+startMinute);
-					System.out.println("End time= " + endHour+":"+endMinute);
-				*/
-				}
-				
-				
+						//System.out.println("CurrentDay: " + currentDay);
+						//System.out.println("Start: " + startHour+":" +startMinute + "\tEnd: " + endHour+":"+endMinute);
+					System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " IS NOT playing!");
+					
+				}//end else
 				i++;
-				
-				
-				
-			}
-		}
-		
+			}//end wile
+		}//end try
+
 		catch(Exception e) {
 			e.printStackTrace();
-		}
-		
+		}//end catch
+
 		BufferedImage[] temp = new BufferedImage[imageList.size()];
 		imageList.toArray(temp);
+		
+		imageNames = new String[names.size()];
+		names.toArray(imageNames);
 		imgs = temp;
 		return temp;
 
-	}
-	
+	}//end getCurrentContent
+
 	class SlideShowFrame extends JFrame {
-		
+
 		public SlideShowFrame() {
 			super();
 			setUndecorated(true);
@@ -329,53 +272,53 @@ public class Signage {
 			ActionListener slideshowChanger = new ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
 					reader.changeImage();
-				}
-			};
+				}//end actionPerformed
+			};//end slideshowChanger
 			Timer timer = new Timer(10000,slideshowChanger);
 			timer.setRepeats(true);
 			timer.start();
-		}
-		
-		
-	
-	
-	class Reader extends Thread {
-		
-		public void run() {
-			
-		}
-		
-		public void changeImage() {
-			if(doneDownloading && imgs != null && imgs.length != 0 ) {
-				if(currentImage >= imgs.length) {
-					currentImage = 0;
-				}
-				try {
-					System.out.println("Setting image to " + currentImage);//+ "  " + imageNames[currentImage]);
-					setImage(imgs[currentImage]);
-					currentImage++;
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-		}
-		
-		}
-	}	
-		
+		}//end SlideShowFrame constructor
+
+
+
+
+		class Reader extends Thread {
+
+			public void run() {
+
+			}//end run
+
+			public void changeImage() {
+				if(doneDownloading && imgs != null && imgs.length != 0 ) {
+					if(currentImage >= imgs.length) {
+						currentImage = 0;
+					}//end if
+					try {
+						System.out.println("Setting image to " + currentImage + ": " + imageNames[currentImage]);//+ "  " + imageNames[currentImage]);
+						setImage(imgs[currentImage]);
+						currentImage++;
+					}//end try
+					catch(Exception e) {
+						e.printStackTrace();
+					}//end catch
+				}//end if
+
+			}//end changeImage
+		}//end Reader class	
+
 		public void setImage(BufferedImage newImage) {
 			if(newImage != null) {
 				image = newImage;
 				repaint();
-			}
-		}
-		
+			}//end if
+		}//end setImage
+
 		public void paint(Graphics g) {
 			if(image != null) {
 				g.drawImage(image,0,0,this);
-			}
-		}
-		
-	
-	}
-}
+			}//end if
+		}//end paint
+
+
+	}//end SlideShowFrame class
+}//end Signage class
