fea3fe7aefbeddf9490c373776fd24abc59271e7
bougelahi <bougelahi@gmail.com>
2014-09-17 14:26:56 -0400

    Deletion and Sockets
    - Added functionality to delete old images from local storage
    - Added functionality to force an update from the Signage website using
    Java Sockets and PHP

--- Signage.java
+++ Signage.java
@@ -18,31 +18,44 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Signage {
 
 	 //Declare variables for the slideshows
 	boolean first = true;
 	String schedule = ""; 
-	BufferedImage[] imgs; //Array for all the images to be played
+	//BufferedImage[] imgs; //Array for all the images to be played
+	ArrayList<BufferedImage> imgs;
 	ArrayList<String> imageNames; //Array to store all of the image names
 	boolean newImages = true;
-	boolean doneDownloading = false; 
+	boolean doneDownloading = false;
+	boolean hasImages = false;
 	SlideShowFrame frame; //Frame for displaying the images
 	int currentImage = 0; //Index of imgs currently being displayed
 	BufferedImage image; //Current Image
 	private int interval;
+	ServerSocket listenSock = null; //the listening server socket
+	Socket sock = null;             //the socket that will actually be used for communication
 	
 
 	//These store the width and height of the screen connected to the Raspberry Pi
@@ -51,7 +64,7 @@ public class Signage {
 	static final int IMG_HEIGHT = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 	
 	 private static final Properties properties = new Properties();
-	 int previousImage = -1;
+	
 	 
 	 
 	 static {
@@ -79,7 +92,7 @@ public class Signage {
 		//This downloads the schedule and images from the web server
 		ActionListener imageUpdater = new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				imageUpdate();
+				imageUpdate(false);
 			}
 		};//end imageUpdater
 		
@@ -91,13 +104,14 @@ public class Signage {
 			e.printStackTrace();
 		}
 		
+		startSocket();
 		imageNames = new ArrayList<String>();
 		interval = Integer.parseInt(properties.getProperty("interval"));
 		//imageUpdate is called every 1 minute
 		Timer timer = new Timer(60000,imageUpdater);
 		timer.setRepeats(true);
 		timer.start();
-		imageUpdate();
+		imageUpdate(false);
 		
 		//create the frame for the slideshow
 		frame = new SlideShowFrame();
@@ -105,16 +119,20 @@ public class Signage {
 	}
 
 	//imageUpdate is called by the ActionListener
-	public void imageUpdate() {
+	public void imageUpdate(boolean forced) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 		String currentTime = sdf.format(new Date());
 		int currentMinute = Integer.parseInt(currentTime.substring(currentTime.indexOf(":") + 1));
-		if(currentMinute % interval == 0 || first) {
-
+		if(currentMinute % interval == 0 || first || forced) {
+			first = false;
+			if(forced)
+				System.out.println("FORCED UPDATE");
+			
+			
 			try {
 				//first attempt to download the schedule to txt file
-				System.out.println("Download Schedule!");
+				//System.out.println("Download Schedule!");
 				downloadSchedule();
 			}//end try
 			catch(Exception e) {
@@ -122,15 +140,16 @@ public class Signage {
 			}//end catch
 			try {
 				//Then download the images in the slideshow
-				if(newImages) {
-					System.out.println("Download Images!");
+			
+					//System.out.println("Download Images!");
 					downloadImages();
-				}
+				
 			}//end try
 			catch(Exception e) {
 				System.out.println("Exception while downloading images");
 			}//end catch
-			first = false;
+			//get the array of images to be played at this moment
+			
 		}//end if
 		else {
 
@@ -159,14 +178,14 @@ public class Signage {
 			
 			HttpURLConnection con = (HttpURLConnection)(url.openConnection());
 			int responseCode = con.getResponseCode();
-			System.out.println("Sending 'GET' request to URL : " + url);
+			//System.out.println("Sending 'GET' request to URL : " + url);
 			System.out.println("Response Code : " + responseCode);
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
 			newImages = false;
-			
+			hasImages = false;
 			//Read in the text from the server one line at a time
 			while((inputLine = in.readLine()) != null) {
 				
@@ -175,11 +194,15 @@ public class Signage {
 				String[] arr = inputLine.split(",");
 				
 				if(arr.length > 1 && arr[0].length() > 1) {
+					hasImages = true;
 					//store image names to download images from the server
-					if(!imageNames.contains(arr[0].substring(1))){
-					imageNames.add(arr[0].substring(1));
-					newImages = true;
-					}
+					
+					String temp = arr[0].substring(1);
+					temp = temp.substring(temp.lastIndexOf("/")+1);
+					if(!imageNames.contains(temp)){
+					imageNames.add(temp);
+					
+				}
 					
 				}
 			}//end while
@@ -187,9 +210,11 @@ public class Signage {
 			in.close();
 			//Store the response in a single string
 			schedule = response.toString();
-			System.out.println(schedule);
+		//	System.out.println(schedule);
+			
 
-			if(newImages) {
+
+		if(schedule.length() > 0) {	
 				//BufferedWriter used to write text to a file
 				BufferedWriter writer = null;
 				writer = new BufferedWriter( new FileWriter("schedule.txt"));
@@ -200,10 +225,9 @@ public class Signage {
 					writer.close( );
 				}//end if
 
-			}
+		}
 			
 		
-		//	imageNameList.toArray(imageNames);
 
 		}//end try 
 		catch (MalformedURLException e) {
@@ -215,6 +239,7 @@ public class Signage {
 			e.printStackTrace();
 		}//end IOExcception catch
 	}//end downloadSchedule
+
 
 	public void downloadImages() {
 
@@ -230,16 +255,15 @@ public class Signage {
 				for(int i = 0; i < imageNames.size(); i++) {
 					try {
 						//Connect to the server using the image names in the URL to download them
-						System.out.println("Writing image: " + imageNames.get(i).substring(imageNames.get(i).lastIndexOf("/")+1));
+						System.out.println("Writing image: " + imageNames.get(i));
 						URL url2	= new URL(properties.getProperty("base") +imageNames.get(i));
-						System.out.println(imageNames.get(i));
 						temp_image = ImageIO.read(url2);
 						type = temp_image.getType() == 0? BufferedImage.TYPE_INT_ARGB : temp_image.getType();
 
 						//Place resized Image inside of the temporary array
 						temp_image = resizeImage(temp_image,type); //Resize
-						if(imageNames.get(i).lastIndexOf("/") != -1) {
-							ImageIO.write(temp_image, "jpg", new File(imageNames.get(i).substring(imageNames.get(i).lastIndexOf("/")+1))); 
+						if(imageNames.get(i).length() > 0 && imageNames.get(i).contains(".jpg")) {
+							ImageIO.write(temp_image, "jpg", new File(imageNames.get(i))); 
 						}//end if
 
 					}//end try
@@ -254,11 +278,13 @@ public class Signage {
 			public void done() {
 
 				//Called once all files have been downloaded, or at least attempted
+				
 				System.out.println("Writing done");
 				doneDownloading = true;
+				getCurrentContent();
+				
 
-				//get the array of images to be played at this moment
-				imgs=getCurrentContent();
+			
 
 			}//end done
 
@@ -269,9 +295,10 @@ public class Signage {
 
 	//This method reads the "schedule.txt" file to get the list of currently
 	//playing images in the slideshow throughout the day
-	public BufferedImage[] getCurrentContent() {
+	public void getCurrentContent() {
 		ArrayList<BufferedImage> imageList = new ArrayList<BufferedImage>();
-		ArrayList<String> names = new ArrayList<String>();
+		imageNames = new ArrayList<String>();
+		//ArrayList<String> names = new ArrayList<String>();
 		try {
 			//Open "schedule.txt" for reading
 			BufferedReader br = new BufferedReader(new FileReader("schedule.txt"));
@@ -287,6 +314,8 @@ public class Signage {
 					attrs = line.split(",");
 
 					if(attrs.length >= 5) {
+						String imageName = attrs[0].substring(attrs[0].lastIndexOf("/")+1);
+						imageNames.add(imageName);
 						SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 						Calendar calendar = Calendar.getInstance();
 
@@ -309,42 +338,72 @@ public class Signage {
 
 						//Compare the actual day/time with the required days/times in the schedule
 						//to determine what should play right now
+						//System.out.println("Start time: " + startHour + ":" + startMinute);
 						if(day == 1 && 
 								((currentHour > startHour && currentHour < endHour) ||
-										(currentHour == startHour && currentMinute >= startMinute) || 
-										(currentHour == startHour && currentHour == endHour && currentMinute >= startMinute && currentMinute <= endMinute)||
-										( currentHour == endHour && currentMinute <= endMinute))){
+								(currentHour == startHour && currentMinute >= startMinute) || 
+								(currentHour == startHour && currentHour == endHour && currentMinute >= startMinute && currentMinute <= endMinute)||
+								( currentHour == endHour && currentMinute <= endMinute))){
 
-							System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " IS playing!");
+							System.out.println(imageName + " IS playing!");
 							if(attrs[0].lastIndexOf("/") != -1){
-
-								imageList.add(ImageIO.read(new File(attrs[0].substring(attrs[0].lastIndexOf("/")+1))));
-								names.add(attrs[0].substring(attrs[0].lastIndexOf("/")+1));
+								try{
+								imageList.add(ImageIO.read(new File(imageName)));
+							//	names.add(attrs[0].substring(attrs[0].lastIndexOf("/")+1));
+								}
+								catch(Exception e) {
+									e.printStackTrace();
+								}
 							}//end if
 						}//end outer if
 						else {
-							System.out.println(attrs[0].substring(attrs[0].lastIndexOf("/")+1) + " IS NOT playing!");
+							System.out.println(imageName + " IS NOT playing!");
 						}//end else
 
 					}//end if
 				}//end wile
 			}//end if
 			br.close();
+		
 		}//end try
 
 		catch(Exception e) {
 			e.printStackTrace();
 		}//end catch
 
+		
+		deleteOldImages();
 		//Store the results and names in arrays
-		BufferedImage[] temp = new BufferedImage[imageList.size()];
-		imageList.toArray(temp);
-
-		//imageNames = names;
-		//return the result
-		return temp;
-
+		imgs = imageList;
+		System.out.println(imgs.size() + " images in slideshow");
+	
+	
 	}//end getCurrentContent
+	
+	public void deleteOldImages() {
+		File dir = new File(System.getProperty("user.dir"));
+		File[] files = dir.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".jpeg") ;
+		    }
+		});
+		
+		for(File f : files) {
+			if(!imageNames.contains(f.getName())) {
+				try {
+					if(f.delete()) {
+						System.out.println(f.getName()  + " deleted successfully!");
+					}
+					else {
+						System.out.println(f.getName()  + " deletion unccessful.");
+					}
+				}
+				catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	//This class contains the code for the Frame/Window of the program
 	class SlideShowFrame extends JFrame {
@@ -366,6 +425,7 @@ public class Signage {
 			displayer.start();
 			ActionListener slideshowChanger = new ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
+		
 					displayer.changeImage();
 				}//end actionPerformed
 			};//end slideshowChanger
@@ -384,16 +444,18 @@ public class Signage {
 
 			//changeImage changes which index of the imgs array to display next
 			public void changeImage() {
-				if(doneDownloading && imgs != null  ) {
+				
+			
+				if(doneDownloading && imgs.size() > 0  ) {
 					
-					if(currentImage >= imgs.length) {
+					if(currentImage >= imgs.size()) {
 						currentImage = 0;
 					}//end if
 					try {
 						
-						System.out.println("Setting image to " + currentImage + ": " + imageNames.get(currentImage));//+ "  " + imageNames[currentImage]);
-						setImage(imgs[currentImage]);
-						previousImage = currentImage;
+						System.out.println("Slideshow image " + currentImage + ": " + imageNames.get(currentImage));//+ "  " + imageNames[currentImage]);
+						setImage(imgs.get(currentImage));
+					
 						currentImage++;
 				
 					}//end try
@@ -428,10 +490,64 @@ public class Signage {
 			}
 		}//end paint
 	}//end SlideShowFrame class
+	
+	public void startSocket() {
+		
+		SwingWorker<Integer, Void> worker = new SwingWorker<Integer, Void>() {
+			@Override
+			public Integer doInBackground() {
+				try {
+			  
+			              listenSock = new ServerSocket(20222);
+			 
+			             while (true) {       //we want the server to run till the end of times
+			            	 
+			                 sock = listenSock.accept();             //will block until connection recieved
+			          
+			                 BufferedReader br =    new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			                 
+			                 String line = "";
+			                 String response = "";
+			                 while ((line = br.readLine()) != null) {
+			                     
+			                     if(line.equals("ForceUpdate"))
+			                    	 imageUpdate(true);
+			                 }
+			
+			                 //Closing streams and the current socket (not the listening socket!)
+			            
+			                 br.close();
+			             }
+			         } catch (IOException ex) {
+			             ex.printStackTrace();
+			        }
+				return 0;
+			}
+		
+		@Override
+		public void done() {
+
+			//Called once all files have been downloaded, or at least attempted
+		      
+               try {
+				sock.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Socket done");
+			
+
+		
+
+		}//end done
+		
+			};
+			worker.execute();
+	}
 
 	public static void main(String[] args) {
 		Signage f = new Signage();
-		f.downloadSchedule();
 
 	}//end main
 }//end Signage class
